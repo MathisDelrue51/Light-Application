39428653d7d7375368671406bded1340f2dd2c25
Mathis <mathis.delrue@gmail.com>
2021-10-06 15:38:11 +0200

    branch colorPicker

--- src/components/Constant/Constant.vue
+++ src/components/Constant/Constant.vue
@@ -1,12 +1,18 @@
 <template>
     <div class="constant">
-        <p>Constant</p>
+        <colorwheel></colorwheel>
+        <div class="btn btn-primary">Valider</div>
     </div>
 </template>
 
 <script>
+import Colorwheel from "../Colorwheel/Colorwheel.vue"
+
 export default {
     name:"Constant",    
+    components:{
+    'colorwheel': Colorwheel,
+  },
 }
 </script>
 
