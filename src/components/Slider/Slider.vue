030e6dce9e38c5e0d12badd19ed133f92c6cf3ee
Mathis <mathis.delrue@gmail.com>
2021-10-06 22:53:05 +0200

    Showing values for the range bars

---
+++ src/components/Slider/Slider.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <vue3-slider v-model="myNumber" />  
+  </div>
+</template>
+
+<script>
+import slider from "vue3-slider"
+
+export default {
+  components: {
+    "vue3-slider": slider
+  }
+}
+</script>
+
+
+<style scoped src="./Slider.css"></style>
